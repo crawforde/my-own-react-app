d6138a2e7a1f1a4705fa27f546397916184b9fa8
crawforde <crawforde@kenyon.edu>
2017-10-06 19:57:06 -0700

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require ('express');
 const app = express();
 const dbRoutes = require('./routes/databaseAccess.js')
+const mongoose = require('mongoose');
 
 // MAKE BUILD FOLDER PUBLICLY AVAILABLE
 app.use(express.static('build'));
+
+// mongoose
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use('/db',dbRoutes);
 
