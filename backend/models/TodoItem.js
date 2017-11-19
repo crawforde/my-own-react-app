578e1fbfc1490f98c278fde10694bf2bbc0d7a06
crawforde <crawforde@kenyon.edu>
2017-11-18 16:42:45 -0800

    Finished

--- backend/models/TodoItem.js
+++ backend/models/TodoItem.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const todoItemSchema = new mongoose.Schema({
-  task: { type: String, required: true },
+  taskName: { type: String, required: true },
   completed: { type: Boolean, default: false }
 });
 
--- backend/routes/databaseAccess.js
+++ backend/routes/databaseAccess.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const TodoItem = require('../models/TodoItem.js');
-//const bodyParser = require('body-parser');
-//router.use(bodyParser.json());
 
 router.post('/add', (req, res) => {
   const testTodo = new TodoItem({
-    task: req.body.data.task
+    taskName: req.body.data.task
   });
   testTodo.save()
     .then(response => {
